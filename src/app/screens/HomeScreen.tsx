d31cc01bfd186bfee6b02a870653fece9915cf72
charlie.castro <ccspart2@gmail.com>
2024-05-08 17:23:51 -0400

    Addition of Expo Image Picker that Allows the app to select a picture from the phone's gallery.

--- src/app/screens/HomeScreen.tsx
+++ src/app/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, ImageBackground, Pressable } from 'react-native';
+import { Text, View, ImageBackground, Pressable } from 'react-native';
 import React from 'react';
 import { Link } from 'expo-router';
 import { useTranslation } from 'react-i18next';
--- src/app/screens/PictureGalleryScreen.tsx
+++ src/app/screens/PictureGalleryScreen.tsx
@@ -1,12 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, Alert } from 'react-native';
 import React from 'react';
 import { Stack } from 'expo-router';
 import { useTranslation } from 'react-i18next';
 import { FC } from 'react';
 import { STYLES } from '@/src/constants';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 const PictureGalleryScreen: FC = () => {
   const { t } = useTranslation();
+
+  const [selectedImageUri, setSelectedImageUri] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setSelectedImageUri(result.assets[0].uri);
+    }
+  };
+
+  function onSignedButtonPressed() {
+    Alert.alert('TODO: Implementation with C2PA signing');
+  }
+
   return (
     <View style={[STYLES.container, { alignItems: 'center' }]}>
       <Stack.Screen
@@ -15,7 +37,25 @@ const PictureGalleryScreen: FC = () => {
           title: t('pictureGallery'),
         }}
       />
-      <Text style={STYLES.title}>{t('pictureGalleryScreen')}</Text>
+      {selectedImageUri ? (
+        <>
+          <Image source={{ uri: selectedImageUri }} style={StyleSheet.absoluteFillObject} />
+          <View style={STYLES.PhotoButtonContainer}>
+            <Pressable style={STYLES.PhotoButtons} onPress={() => setSelectedImageUri(null)}>
+              <Text style={STYLES.photoText}>{t('retake')}</Text>
+            </Pressable>
+            <Pressable style={STYLES.PhotoButtons} onPress={onSignedButtonPressed}>
+              <Text style={STYLES.photoText}>{t('sign')}</Text>
+            </Pressable>
+          </View>
+        </>
+      ) : (
+        <>
+          <Pressable style={[STYLES.button, { padding: 12 }]} onPress={pickImage}>
+            <Text style={STYLES.text}>{t('imagePickerButtonLabel')}</Text>
+          </Pressable>
+        </>
+      )}
     </View>
   );
 };
